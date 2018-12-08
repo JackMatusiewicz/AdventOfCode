b39e9a8f7a12592ad99bc74da6836f8bffd3977b
Jack Matusiewicz <jack.matusiewicz@gmail.com>
2018-12-08 21:12:40 +0000

    Code for Q1 part 1

--- AdventOfCode_2018/AdventOfCode_2018/Program.fs
+++ AdventOfCode_2018/AdventOfCode_2018/Program.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open AdventOfCode
 
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
+let main argv =
+    let r = ProblemOne.result ()
+    printfn "%d" r
     0 // return an integer exit code
